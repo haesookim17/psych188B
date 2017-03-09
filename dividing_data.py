a825e794bff42cf43822346420f0aa6f0b7cb0eb
bryce-wong <super.bryce@gmail.com>
2017-03-08 20:51:24 -0800

    Update dividing_data.py

--- dividing_data.py
+++ dividing_data.py
@@ -1,54 +1,60 @@
-# divde_data for a single subject
+################################################################################
+# @author Bryce Wong
+# 
+# Convert the data into a usable form for a single subject
+################################################################################
+
 def divide_data(labels, features):
-	#INPUT parameters are the data for one subject
+	# @ param labels are the labels for one subject
+	# @ param features are the features for one subject
 	#subject = np.hstack((labels, features)) #combine into one matrix 
 	
-	#OUTPUT parameter is the cleaned matrix for the subject 
-	#this final output is a matrix where the first two columns are the label and the chunk number (ie, "y")
-	#the rest of the columns are feature values (ie, "X")
-	#each row in the matrix corresponds to a single trial in a chunk that has been averaged from all timepoints for that trial
-	#a trial is basically one attempt at a categorization
+	# @ output is the cleaned matrix for the subject 
+	# This final output is a matrix where the first two columns are the label (ie, "y") and the chunk number 
+	# The rest of the columns are feature values (ie, "X")
+	# Each row in the matrix corresponds to a single trial in a chunk that has been averaged from all timepoints for that trial
+	# A trial is basically one attempt at a categorization
 	
-	#intialize a temp variable below
+	# Intialize a temp variable below
 	big_mat = []
 	
 	import numpy as np
 
-	#create a list just to iterate through
-	chunks = list(range(0,11)) #the number of possible chunks
+	# Create a list just to iterate through
+	chunks = list(range(0,11)) # The number of possible chunks
 	
-	for chunk in chunks: #repeat for every chunk
-		#pull out chunk based on the current chunk number being looked at
+	for chunk in chunks: # Repeat for every chunk
+		# Pull out chunk based on the current chunk number being looked at
 		current_chunk = split2chunk(labels,features,chunk)
 		#current_chunk = subject[subject[:,1] == chunk]
 	
-		#create a list of the categories to iterate through 
+		# Create a list of the categories to iterate through 
 		categories = ['face','cat','bottle','shoe','scissor','house','chair','scramblepix']	
 	
-		for category in categories: #repeat for every category within each chunk
-			#for each chunk, pull out subset based on the category label
+		for category in categories: # Repeat for every category within each chunk
+			# For each chunk, pull out subset based on the current category label being looked out
 			current_cat = split2cat(category,current_chunk)
 			#current_cat = current_chunk[current_chunk[:,0]==category]
 
-			#transform each category matrix into a numpy array
+			# Transform each category matrix into a numpy array
     			current_cat = np.array(current_cat)
 		
-    			#average each numpy array into a single row
+    			# Average each category numpy array into a single row
     			current_cat = np.mean(current_cat, axis=0)
 			
-			#add to big matrix holding averaged trials for each category from each chunk
+			# Add to big matrix holding averaged trials for each category from each chunk
 			big_mat.append(current_cat)
 		
-	#convert final output to numpy array
+	# Convert final output to numpy array
 	big_mat =np.array(big_mat)
 	
-	#subtract the averaged rest values from every trial
-	avg_rest = get_rest(labels,features) #get vector of average rest values
-	big_mat_features = big_mat[:,2:] #get only the features from our final cleaned data
-	big_mat_labels = big_mat[:,0:2] #also store only the labels to put together with final features later
+	# Subtract the averaged rest values from every trial
+	avg_rest = get_rest(labels,features) # Get vector of average rest values
+	big_mat_features = big_mat[:,2:] # Get only the features from our final cleaned data
+	big_mat_labels = big_mat[:,0:2] # Also store only the labels to put together with final features later
 	final_features = big_mat_features - avg_rest
 	
-	#put final labels and features together
+	# Put final labels and features together
 	final_data = np.hstack((big_mat_labels, final_features))
 	
 	return final_data
