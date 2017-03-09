3f361168c5793281afc6d49dafd1a0e9ba474cf5
bryce-wong <super.bryce@gmail.com>
2017-03-08 20:31:53 -0800

    Update dividing_data.py

--- dividing_data.py
+++ dividing_data.py
@@ -1,41 +1,49 @@
 # divde_data for a single subject
-
-def divide_data(subject):
-	#input parameter is the data of one subject
+def divide_data(labels, features):
+	#input parameters are the data for one subject
+	#subject = np.hstack((labels, features)) #combine into one matrix 
 	
-	#output parameter is the cleaned matrix for the subject - intialize below
+	#output parameter is the cleaned matrix for the subject - intialize a temp variable below
 	big_mat = []
 	
 	import numpy as np
 
 	#create a list just to iterate through
-	chunks = list(range(0,11))
+	chunks = list(range(0,11)) #the number of possible chunks
 	
-	for chunk in chunks:
-		#pull out chunk based on the chunk number there
-		current_chunk = split2chunk(subject['labels'],subject['features'],chunk)
+	for chunk in chunks: #repeat for every chunk
+		#pull out chunk based on the current chunk number being looked at
+		current_chunk = split2chunk(labels,features,chunk)
+		#current_chunk = subject[subject[:,1] == chunk]
 	
-		#create a list to iterate through
-		labels = ['face','cat','bottle','shoe','scissor','house','chair','scramblepix']	
+		#create a list of the categories to iterate through 
+		categories = ['face','cat','bottle','shoe','scissor','house','chair','scramblepix']	
 	
-		for label in labels:
-			#for each chunk, pull out subset based on the label
-			current_label = current_chunk[current_chunk[:,0]==label]
+		for category in categories: #repeat for every category within each chunk
+			#for each chunk, pull out subset based on the category label
+			current_cat = split2cat(category,current_chunk)
+			#current_cat = current_chunk[current_chunk[:,0]==category]
 
 			#transform each category matrix into a numpy array
-    			current_label = np.array(current_label)
+    			current_cat = np.array(current_cat)
 		
     			#average each numpy array into a single row
-    			current_label = np.mean(current_label, axis=0)
+    			current_cat = np.mean(current_cat, axis=0)
 			
-			#add to big matrix holding averaged trials for each label from each chunk
-			big_mat.append(current_label)
+			#add to big matrix holding averaged trials for each category from each chunk
+			big_mat.append(current_cat)
 		
-	#subtract the averaged rest values from every trial
-	avg_rest = get_rest(subject['labels'],subject['features'])
-	
 	#convert final output to numpy array
 	big_mat =np.array(big_mat)
 	
-	return big_mat
+	#subtract the averaged rest values from every trial
+	avg_rest = get_rest(labels,features) #get vector of average rest values
+	big_mat_features = big_mat[:,2:] #get only the features from our final cleaned data
+	big_mat_labels = big_mat[:,0:2] #also store only the labels to put together with final features later
+	final_features = big_mat_features - avg_rest
+	
+	#put final labels and features together
+	final_data = np.hstack((big_mat_labels, final_features))
+	
+	return final_data
 	
