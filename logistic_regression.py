7dd2667ea9f2dc774290c9669b5eaa38c924c87b
bryce-wong <super.bryce@gmail.com>
2017-03-12 16:52:14 -0700

    Update logistic_regression.py

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,20 +1,24 @@
 ###################################################################################
 #
 # By Bryce
-# Run a logistic regression 
+# Run a logistic regression model
 ##################################################################################
 def logistic_regression(labels,features):
   #@ param labels are the labels of one subject's cleaned/preprocessed dataset
   #@ param features are the features of one subject's cleaned/preprocessed dataset
+  
+  #split into training and testing set
+  from sklearn.model_selection import train_test_split
+  X_train, X_test, y_train, y_test = train_test_split(features, labels[;,0], test_size=.2)
     
-  # Import our scikit-learn functions
+  # Import our scikit-learn function
   from sklearn.linear_model import LogisticRegression
   
   # Create our logistic regression model
   logreg = LogisticRegression()
   
   # Train our model on our data
-  logreg.fit(preProc_X, y)
+  logreg.fit(X_train, y_train)
   y_pred_lr = logreg.decision_function(test_X)
   
   # Test our model and score it
