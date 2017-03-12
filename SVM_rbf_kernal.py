4f32590817da86c63404263d03c009dae56f7373
bryce-wong <super.bryce@gmail.com>
2017-03-12 16:32:06 -0700

    Update SVM_rbf_kernal.py

--- SVM_rbf_kernal.py
+++ SVM_rbf_kernal.py
@@ -13,6 +13,10 @@ def SVM_rbf_kernel(labels, features):
   from sklearn.grid_search import GridSearchCV
   from sklearn.cross_validation import StratifiedKFold
   
+  #split into training and testing set
+  from sklearn.model_selection import train_test_split
+  X_train, X_test, y_train, y_test = train_test_split(features, labels[;,0], test_size=.2)
+  
   # Set a range of possible values for our C parameter and gamma parameter to iterate through
   possible_C = np.logspace(-3, 9, 13)
   possible_gamma = np.logspace(-7, 4, 12)
@@ -21,25 +25,25 @@ def SVM_rbf_kernel(labels, features):
   param_grid = dict(gamma=possible_gamma, C=possible_C)
   
   # Create our cross-validation function
-  cv = StratifiedKFold(labels[:,0], 10) # Uses our labels as our y-vector, makes 10 folds
+  cv = StratifiedKFold(y_train, 10) # Uses our labels as our y-vector, makes 10 folds
   
   # Create our svm model
   svc = SVC()
   
   # Cross-validate our parameters in our grid to find best combination of the params
   grid = GridSearchCV(svc, param_grid=param_grid, cv=cv)
-  grid.fit(features, labels[:,0])
+  grid.fit(X_train, y_train)
   #print(grid.best_params_)
 
   # Create our svm model with rbf kernels using our optimal params
   svc_rbf = SVC(**grid.best_params_, kernel="rbf")
-  svc_rbf.fit(features, labels[:,0])
+  svc_rbf.fit(X_train, y_train)
 
   # Turn it into a pipeline if we want?
   svc_rbf_model = pp.make_pipeline(preprocessor, SVC(**grid.best_params_, kernel="rbf"))
 
   # Fit our pipelined SVM model if we want?
-  svc_rbf_model.fit(features, labels[:,0])
+  svc_rbf_model.fit(X_train, y_train)
   y_pred_svc = svc_rbf_model.decision_function(test_X)
   score1 = svc_rbf_model.score(test_X, test_y)
   print("The SVC model with RBF kernals has an accuracy score of:", score1)
