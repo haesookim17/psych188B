cdf83207e246d8367484d964ebfd86045769454a
haesookim17 <haesookim17@gmail.com>
2017-03-12 17:46:13 -0700

    Update logistic_regression.py

--- logistic_regression.py
+++ logistic_regression.py
@@ -4,25 +4,25 @@
 # Run a logistic regression model
 ##################################################################################
 def logistic_regression(labels,features):
-  #@ param labels are the labels of one subject's cleaned/preprocessed dataset
-  #@ param features are the features of one subject's cleaned/preprocessed dataset
-  
-  #split into training and testing set
-  from sklearn.model_selection import train_test_split
-  X_train, X_test, y_train, y_test = train_test_split(features, labels[;,0], test_size=.2)
+    #@ param labels are the labels of one subject's cleaned/preprocessed dataset
+    # #@ param features are the features of one subject's cleaned/preprocessed dataset
     
-  # Import our scikit-learn function
-  from sklearn.linear_model import LogisticRegression
+    #split into training and testing set
+    from sklearn.model_selection import train_test_split
+    X_train, X_test, y_train, y_test = train_test_split(features, labels[:,0], test_size=.2)
+    
+    # Import our scikit-learn function
+    from sklearn.linear_model import LogisticRegression
   
-  # Create our logistic regression model
-  logreg = LogisticRegression()
+    # Create our logistic regression model
+    logreg = LogisticRegression()
   
-  # Train our model on our data
-  logreg.fit(X_train, y_train)
-  y_pred_lr = logreg.decision_function(test_X)
+    # Train our model on our data
+    logreg.fit(X_train, y_train)
+    y_pred_lr = logreg.decision_function(X_test)
   
-  # Test our model and score it
-  score3 = logreg.score(test_X, test_y)
-  print("The logistic regression model has an accuracy score of:", score3)
+    # Test our model and score it
+    score3 = logreg.score(X_test, y_test)
+    print("The logistic regression model has an accuracy score of:", score3)
   
-  return
+    return
